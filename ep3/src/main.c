1b66eea1169a7d188b6f079eb214ccf5dcdd2faf
Fabio Pallini <fabiopallini01@gmail.com>
2024-01-31 00:18:46 +0100

    player2 input

--- ep3/src/main.c
+++ ep3/src/main.c
@@ -1,7 +1,7 @@
 #include "psx.h"
 
 unsigned long *cd_data;
-Sprite sprite;
+Sprite player1, player2;
 DR_MODE dr_mode;
 SPRT sprt;
 
@@ -10,10 +10,10 @@ void sprt_init(DR_MODE *dr_mode, SPRT *sprt){
 	SetSprt(sprt);
 	sprt->u0 = 0; 
 	sprt->v0 = 0;
-	sprt->w = 32; 
-	sprt->h = 32;
+	sprt->w = 41; 
+	sprt->h = 46;
 	setRGB0(sprt, 255, 255, 255);
-	setXY0(sprt, 50, 50);
+	setXY0(sprt, 10, 20);
 }
 
 int main() {
@@ -24,22 +24,47 @@ int main() {
 
 	sprt_init(&dr_mode, &sprt);
 
-	sprite_init(&sprite, 41, 46, cd_data);
-	sprite_set_uv(&sprite, 41, 0, 41, 46);
-	sprite.direction = 1;
-	sprite.pos.vx = 50;
-	sprite.pos.vy = 50; 
+	sprite_init(&player1, 41, 46, cd_data);
+	player1.direction = 1;
+	sprite_set_uv(&player1, 41, 0, 41, 46);
+	player1.pos.vx = 180;
+	player1.pos.vy = 180; 
+
+	sprite_init(&player2, 41, 46, cd_data);
+	player2.direction = 1;
+	sprite_set_uv(&player2, 41, 0, 41, 46);
+	player2.pos.vx = 180;
+	player2.pos.vy = 180; 
 
 	while(1) {
 		psClear();
 
-		sprite_anim(&sprite, 41, 46, 0, 0, 6);
-		sprite.pos.vx += 2;
-		if(sprite.pos.vx >= 320)	
-			sprite.pos.vx = 0;
+		sprite_anim(&player1, 41, 46, 0, 0, 6);
+		player1.pos.vx += 2;
+		if(player1.pos.vx >= 320)	
+			player1.pos.vx = 0;
+
+		if((pad >> 16 & PADLleft) == 0 && (pad >> 16 & PADLright) == 0)
+			sprite_set_uv(&player2, 0, 46*2, 41, 46);
+			
+		if(pad >> 16 & PADLleft){
+			player2.direction = 0;
+			sprite_anim(&player2, 41, 46, 0, 0, 6);
+			player2.pos.vx -= 2;
+		}
+		if(pad >> 16 & PADLright){
+			player2.direction = 1;
+			sprite_anim(&player2, 41, 46, 0, 0, 6);
+			player2.pos.vx += 2;
+		}
+		if(player2.pos.vx + player2.w < 0)	
+			player2.pos.vx = SCREEN_WIDTH;
+		if(player2.pos.vx > SCREEN_WIDTH)	
+			player2.pos.vx = 0;
 
 		FntPrint("hello world");
-		drawSprite_2d(&sprite);
+		drawSprite_2d(&player1);
+		drawSprite_2d(&player2);
 		drawSprt(&dr_mode, &sprt);
 
 		psDisplay();
