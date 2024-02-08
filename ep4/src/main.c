8bce5e1e578e2a6d18cc5c58ba664dcc76d3ebc8
Fabio Pallini <fabiopallini01@gmail.com>
2024-02-09 00:32:22 +0100

    blocks and gravity

--- ep4/src/main.c
+++ ep4/src/main.c
@@ -1,27 +1,19 @@
 #include "psx.h"
 
+#define n_blocks 41 
+
 u_long *cd_data[2];
 u_short tpages[2];
 Sprite player[2];
-DR_MODE dr_mode;
-SPRT sprt;
+char fall[2];
 
 typedef struct {
 	DR_MODE dr_mode;
 	SPRT sprt;
 } BLOCK;
-BLOCK blocks[5];
+BLOCK blocks[n_blocks];
 
-void sprt_init(DR_MODE *dr_mode, SPRT *sprt){
-	SetDrawMode(dr_mode, 0, 0, GetTPage(2, 0, 768, 0), 0);
-	SetSprt(sprt);
-	sprt->u0 = 0; 
-	sprt->v0 = 0;
-	sprt->w = 14; 
-	sprt->h = 7;
-	setRGB0(sprt, 255, 255, 255);
-	setXY0(sprt, 20, 20);
-}
+void gravity(Sprite *s, int n);
 
 void block_init(BLOCK *b){
 	SetDrawMode(&b->dr_mode, 0, 0, GetTPage(2, 0, 768, 0), 0);
@@ -35,6 +27,7 @@ void block_init(BLOCK *b){
 
 int main() {
 	int i = 0;
+	int k = 0;
 	psSetup();
 	cd_open();
 	cd_read_file("PLAYER1.TIM", &cd_data[0]);
@@ -45,74 +38,119 @@ int main() {
 	tpages[1] = loadToVRAM(cd_data[1]);
 	//free3(cd_data);
 
-	sprt_init(&dr_mode, &sprt);
-
 	for(i = 0; i <= 1; i++){
-		sprite_init(&player[i], 41, 46, tpages[0]);
+		sprite_init(&player[i], 31, 36, tpages[0]);
 		player[i].direction = 1;
 		sprite_set_uv(&player[i], 41, 0, 41, 46);
-		player[i].pos.vx = 20;
-		player[i].pos.vy = SCREEN_HEIGHT - player[i].h; 
+		player[i].pos.vy = -50;
 	}
+	player[0].pos.vx = 35;
+	player[1].pos.vx = SCREEN_WIDTH - 100; 
 
-	for(i = 0; i < 5; i++){
+	for(i = 0; i < n_blocks; i++){
 		block_init(&blocks[i]);
-		setXY0(&blocks[i].sprt, 20+(15*i), 100);
+		if(i <= 3)
+			setXY0(&blocks[i].sprt, 40+(15*i), 50*1);
+		if(i > 3 && i <= 7)
+			setXY0(&blocks[i].sprt, SCREEN_WIDTH-(15*i), 50*1);
+
+		if(i > 7 && i <= 13)
+			setXY0(&blocks[i].sprt, 10+(15*k++), 50*2);
+		if(i > 13 && i <= 17)
+			setXY0(&blocks[i].sprt, 40+(15*k++), 50*2);
+		if(i > 17 && i <= 21)
+			setXY0(&blocks[i].sprt, 65+(15*k++), 50*2);
+
+		if(i == 22)
+			k = 0;
+		if(i > 21 && i <= 28)
+			setXY0(&blocks[i].sprt, 25+(15*k++), 50*3);
+		if(i > 28 && i <= 32)
+			setXY0(&blocks[i].sprt, 45+(15*k++), 50*3);
+
+		if(i == 33)
+			k = 0;
+		if(i > 32 && i <= 36)
+			setXY0(&blocks[i].sprt, 35+(15*k++), 50*5);
+		if(i > 36 && i <= 40)
+			setXY0(&blocks[i].sprt, SCREEN_WIDTH-(15*k++), 50*5);
 	}
 
 	while(1) {
 		int i = 0;
 		psClear();
+	
+		gravity(&player[0], 0);
+		gravity(&player[1], 1);
 
 		// PLAYER 1 INPUT
-		if((pad & PADLleft) == 0 && (pad & PADLright) == 0)
-			sprite_set_uv(&player[0], 0, 46*1, 41, 46);
-			
-		if(pad & PADLleft){
-			player[0].direction = 0;
-			sprite_anim(&player[0], 41, 46, 0, 0, 6);
-			player[0].pos.vx -= 2;
+		if(fall[0] == 0) {
+			if((pad & PADLleft) == 0 && (pad & PADLright) == 0)
+				sprite_set_uv(&player[0], 0, 46*1, 41, 46);
+				
+			if(pad & PADLleft){
+				player[0].direction = 0;
+				sprite_anim(&player[0], 41, 46, 0, 0, 6);
+				player[0].pos.vx -= 2;
+			}
+			if(pad & PADLright){
+				player[0].direction = 1;
+				sprite_anim(&player[0], 41, 46, 0, 0, 6);
+				player[0].pos.vx += 2;
+			}
+			if(player[0].pos.vx + player[0].w < 0)	
+				player[0].pos.vx = SCREEN_WIDTH;
+			if(player[0].pos.vx > SCREEN_WIDTH)	
+				player[0].pos.vx = 0;
 		}
-		if(pad & PADLright){
-			player[0].direction = 1;
-			sprite_anim(&player[0], 41, 46, 0, 0, 6);
-			player[0].pos.vx += 2;
-		}
-		if(player[0].pos.vx + player[0].w < 0)	
-			player[0].pos.vx = SCREEN_WIDTH;
-		if(player[0].pos.vx > SCREEN_WIDTH)	
-			player[0].pos.vx = 0;
-
 		// PLAYER 2 INPUT
-		if((pad2 & PADLleft) == 0 && (pad2 & PADLright) == 0)
-			sprite_set_uv(&player[1], 0, 46*1, 41, 46);
-			
-		if(pad2 & PADLleft){
-			player[1].direction = 0;
-			sprite_anim(&player[1], 41, 46, 0, 0, 6);
-			player[1].pos.vx -= 2;
+		if(fall[1] == 0) {
+			if((pad2 & PADLleft) == 0 && (pad2 & PADLright) == 0)
+				sprite_set_uv(&player[1], 0, 46*1, 41, 46);
+				
+			if(pad2 & PADLleft){
+				player[1].direction = 0;
+				sprite_anim(&player[1], 41, 46, 0, 0, 6);
+				player[1].pos.vx -= 2;
+			}
+			if(pad2 & PADLright){
+				player[1].direction = 1;
+				sprite_anim(&player[1], 41, 46, 0, 0, 6);
+				player[1].pos.vx += 2;
+			}
+			if(player[1].pos.vx + player[1].w < 0)	
+				player[1].pos.vx = SCREEN_WIDTH;
+			if(player[1].pos.vx > SCREEN_WIDTH)	
+				player[1].pos.vx = 0;
 		}
-		if(pad2 & PADLright){
-			player[1].direction = 1;
-			sprite_anim(&player[1], 41, 46, 0, 0, 6);
-			player[1].pos.vx += 2;
-		}
-		if(player[1].pos.vx + player[1].w < 0)	
-			player[1].pos.vx = SCREEN_WIDTH;
-		if(player[1].pos.vx > SCREEN_WIDTH)	
-			player[1].pos.vx = 0;
-
 		FntPrint("hello world");
 		drawSprite_2d(&player[0]);
 		drawSprite_2d(&player[1]);
 
-		drawSprt(&dr_mode, &sprt);
-		for(i = 0; i < 5; i++)
+		for(i = 0; i < n_blocks; i++)
 			drawSprt(&blocks[i].dr_mode, &blocks[i].sprt);
 
 		psDisplay();
 	}
 
 	return 0;
+}
+
+void gravity(Sprite *s, int n){
+	int i = 0;
+	int margin = 10;
+	fall[n] = 1; // falling down
+	for(i = 0; i < n_blocks; i++){
+		if(s->pos.vx + (s->w-margin) >= blocks[i].sprt.x0 && 
+		s->pos.vx + margin <= blocks[i].sprt.x0 + blocks[i].sprt.w &&
+		s->pos.vy + s->h >= blocks[i].sprt.y0 && 
+		s->pos.vy + s->h <= blocks[i].sprt.y0 + 1) 
+		{
+			fall[n] = 0;
+			break;
+		}
+	}
+	if(fall[n] == 1 && s->pos.vy < SCREEN_HEIGHT - s->h)
+		s->pos.vy += 2;
 }
 
